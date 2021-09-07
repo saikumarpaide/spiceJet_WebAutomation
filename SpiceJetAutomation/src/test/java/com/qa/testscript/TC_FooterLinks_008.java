ac3c4a94bb14e8c95a01741f16eaa9147801319f
saikumar <saikumarpaide796@gmail.com>
2021-09-07 23:29:48 +0530

    Footer Commit

--- SpiceJetAutomation/src/test/java/com/qa/testscript/TC_FooterLinks_008.java
+++ SpiceJetAutomation/src/test/java/com/qa/testscript/TC_FooterLinks_008.java
@@ -1,5 +1,51 @@
 package com.qa.testscript;
 
-public class TC_FooterLinks_008 {
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import com.qa.pages.FooterLinksPage;
+
+public class TC_FooterLinks_008 extends TestBase{
+	
+	FooterLinksPage pages;
+	int count =1; 
+	@Test
+	public void checkFooterLinks() throws InterruptedException {
+		System.out.println(count++);
+		/*
+		 * Alert alert = driver.switchTo().alert(); System.out.println("FJDJG");
+		 * alert.accept();
+		 */pages = new FooterLinksPage(driver);
+		System.out.println(count++);
+		Actions action = new Actions(driver);
+		
+		action.moveToElement(pages.getHomeLink()).click();
+		Thread.sleep(5000);
+		System.out.println(count++);
+		
+		action.moveToElement(pages.getPrivacyPolicyLink()).click();
+		Thread.sleep(5000);
+		System.out.println(count++);
+
+		action.moveToElement(pages.getTermsAndConditions()).click();
+		Thread.sleep(5000);
+		System.out.println(count++);
+
+		action.moveToElement(pages.getGstInformation()).click();
+		Thread.sleep(5000);
+		System.out.println(count++);
+
+		action.moveToElement(pages.getSiteMap()).click();
+		Thread.sleep(5000);
+		System.out.println(count++);
+
+		
+		
+		
+		
+		 
+		
+	}
 
 }
--- SpiceJetAutomation/src/test/java/com/qa/testscript/TestBase.java
+++ SpiceJetAutomation/src/test/java/com/qa/testscript/TestBase.java
@@ -1,9 +1,13 @@
 package com.qa.testscript;
 
-import org.openqa.selenium.Alert;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -14,13 +18,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
 	LoginAndSignupPage loginandSignupPage;
+	
 	public WebDriver driver;
-	@Parameters("SignupURL")
+	@Parameters("url")
 	@BeforeClass
 	public void setUp(String url) {
-		WebDriverManager.edgedriver().setup();
+		WebDriverManager. edgedriver().setup();
 		driver=new EdgeDriver();
 		driver.get(url);
+		//chromePop();
 		driver.manage().window().maximize();
 		//driver.switchTo().alert().accept();
 		System.out.println("Before Suite");
@@ -33,6 +39,30 @@ public class TestBase {
 		}
 	}
 		
+	public void firefoxpopupDisbled(){
+		FirefoxProfile geoDisabled = new FirefoxProfile();
+		geoDisabled.setPreference("geo.enabled", false);
+		geoDisabled.setPreference("geo.provider.use_corelocation", false);
+		geoDisabled.setPreference("geo.prompt.testing", false);
+		geoDisabled.setPreference("geo.prompt.testing.allow", false);
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		capabilities.setCapability(FirefoxDriver.PROFILE, geoDisabled);
+		driver = new FirefoxDriver(capabilities);
+		
+	}
+	
+	@SuppressWarnings("null")
+	public void chromePop() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		options.addArguments("test-type");
+		options.addArguments("enable-strict-powerful-feature-restrictions");
+		options.addArguments("disable-geolocation");
+		MutableCapabilities cap = null;
+		cap.setCapability(ChromeOptions.CAPABILITY, options);
+		cap = cap.merge(DesiredCapabilities.chrome());
+		driver = new ChromeDriver(cap);
+	}
 	
 
 }
